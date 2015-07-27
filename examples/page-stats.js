c5c1e27e162482f58fd9eeee1fab992700d45f92
Brent Jett <brentjett@users.noreply.github.com>
2015-07-26 22:23:51 -0500

    Update page-stats.js

--- examples/page-stats.js
+++ examples/page-stats.js
@@ -1,4 +1,5 @@
 /*
+@Title: General Page Stats
 @subject: Page
 
 Page.title;
@@ -7,8 +8,18 @@ Page.elements; // dom
 
 */
 
+// Display page metadata
+var metas = Page.elements.find("meta");
+metas.each(function(element){
+  Response.addObject({
+    
+  });
+});
+
 // Count outbound links
 var outbound_links = Page.elements.find("a[href*='://']");
 var total_outbound_links = outbound_links.length;
-
 Response.addItem("Total Outbound Links", total_outbound_links);
+
+// Count tel: links
+// Count mailto: links
